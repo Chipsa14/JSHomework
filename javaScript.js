153c443c426d8a743c91e13b7738fc90e077772d
OLEKSANDR RASHEVCHENKO <135847896+Chipsa14@users.noreply.github.com>
2023-08-25 12:41:10 +0200

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,66 @@
+let prise = +prompt();
+let discond = .10;
+
+console.log(prise * 0.9);
+
+
+
+
+
+let prise =+prompt();
+let margin =10;
+console.log(prise*1.1);
+
+
+let prise = +prompt();
+let discond = 25;
+console.log (prise*0.75);
+
+
+let prise =+prompt();
+let margin =25;
+console.log(prise*1.25)
+
+
+
+
+undefined
+let prise = +prompt();
+let discond = 1;
+
+console.log(prise * 0.99);
+
+
+
+
+
+let cost = +prompt();
+let discond = 0.5;
+
+if (cost>=100) {
+    let diffrense = cost * 0.95;
+    alert(diffrense);
+    
+}
+else{
+    alert(cost);
+}
+
+
+
+
+let userNumber = +prompt();
+
+
+if (userNumber > 0) {
+    console.log("Число больше нуля.");
+} else if (userNumber < 0) {
+    console.log("Число меньше нуля.");
+} else {
+    console.log("Число равно нулю.");
+}
+
+
+let number =+prompt();
+
+console.log(number%10);
