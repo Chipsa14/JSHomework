993c23aae8fc08b69451d41084fe73d67ad3d656
OLEKSANDR RASHEVCHENKO <135847896+Chipsa14@users.noreply.github.com>
2023-08-25 14:30:35 +0200

    Add files via upload

--- javaScript.js
+++ javaScript.js
@@ -3,7 +3,7 @@ let discond = .10;
 
 console.log(prise * 0.9);
 
-
+---------------------------------------------------
 
 
 
@@ -11,12 +11,12 @@ let prise =+prompt();
 let margin =10;
 console.log(prise*1.1);
 
-
+-----------------------------------------------
 let prise = +prompt();
 let discond = 25;
 console.log (prise*0.75);
 
-
+-----------------------------------------------------
 let prise =+prompt();
 let margin =25;
 console.log(prise*1.25)
@@ -24,7 +24,7 @@ console.log(prise*1.25)
 
 
 
-undefined
+----------------------------------------
 let prise = +prompt();
 let discond = 1;
 
@@ -33,7 +33,7 @@ console.log(prise * 0.99);
 
 
 
-
+-----------------------------------------
 let cost = +prompt();
 let discond = 0.5;
 
@@ -48,7 +48,7 @@ else{
 
 
 
-
+----------------------------------------
 let userNumber = +prompt();
 
 
@@ -60,7 +60,26 @@ if (userNumber > 0) {
     console.log("Число равно нулю.");
 }
 
-
+----------------------------------
 let number =+prompt();
 
 console.log(number%10);
+
+
+
+--------------------------
+
+1 double_sum
+2 average_value
+3 even_number
+4 prime_number
+5 comfortable_view
+6 uppercase_first
+7 priority_in_masiv
+8 random_generate
+9 is_email_tru
+10 highest_value
+11 smallest_value
+12 area_of_figure
+13 factorial_of_number
+14 distance_calculator
